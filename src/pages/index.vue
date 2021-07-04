df815f987aee7f2462a481cb813755ede0ece376
Rafael Takei <rafael@suthub.com>
2021-07-04 01:14:02 -0300

    first commit

---
+++ src/pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>Test index</div>
+  <hello-world />
+</template>
+
+<script>
+// import HelloWorld from "../components/HelloWorld.vue"
+export default {
+  // components: {
+  //   HelloWorld,
+  // },
+  setup() {
+    return {}
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
